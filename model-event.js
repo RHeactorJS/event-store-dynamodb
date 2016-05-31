aca8fb2521335a428b8b43ef3b78399f439fd70e
Markus Tacker <m@cto.hiv>
2016-05-31 11:15:39 +0200

    feat(ModelEvent): add createdAt field

--- model-event.js
+++ model-event.js
@@ -5,11 +5,13 @@
  *
  * @param {String} aggregateId
  * @param {object} data
+ * @param {Number} createdAt
  * @constructor
  */
-function ModelEvent (aggregateId, data) {
+function ModelEvent (aggregateId, data, createdAt) {
   Object.defineProperty(this, 'aggregateId', {value: aggregateId, enumerable: true})
   Object.defineProperty(this, 'data', {value: data || {}, enumerable: true})
+  Object.defineProperty(this, 'createdAt', {value: createdAt || Date.now(), enumerable: true})
 }
 
 module.exports = ModelEvent
