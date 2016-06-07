86cd0713d0a5ee763a07a70bf3e8b3d98c5f8c74
Markus Tacker <m@cto.hiv>
2016-06-07 13:20:24 +0200

    feat(ModelEvent): add aggregateVersion

--- model-event.js
+++ model-event.js
@@ -3,15 +3,17 @@
 /**
  * Models should return and instance of this model from their setters
  *
- * @param {String} aggregateId
- * @param {object} data
- * @param {Number} createdAt
+ * @param {String} aggregateId ID of the aggregate
+ * @param {Number} aggregateVersion New version of the aggregate
+ * @param {Number} createdAt Time of event creation, defaults to Date.now()
+ * @param {object} data Necessary data to replay the event
  * @constructor
  */
-function ModelEvent (aggregateId, data, createdAt) {
+function ModelEvent (aggregateId, aggregateVersion, createdAt, data) {
   Object.defineProperty(this, 'aggregateId', {value: aggregateId, enumerable: true})
-  Object.defineProperty(this, 'data', {value: data || {}, enumerable: true})
+  Object.defineProperty(this, 'aggregateVersion', {value: aggregateVersion, enumerable: true})
   Object.defineProperty(this, 'createdAt', {value: createdAt || Date.now(), enumerable: true})
+  Object.defineProperty(this, 'data', {value: data || {}, enumerable: true})
 }
 
 module.exports = ModelEvent
