4d32ed288ff71ae61f9faa36eba7f63cf10b29bf
Markus Tacker <m@coderbyheart.com>
2017-02-07 17:05:05 +0100

    fix(package): export SnapshotAggregateRepository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export * from './aggregate-index'
 export * from './aggregate-relation'
 export * from './aggregate-repository'
+export * from './snapshot-aggregate-repository'
 export * from './aggregate-root'
 export * from './event-store'
 export * from './model-event'
