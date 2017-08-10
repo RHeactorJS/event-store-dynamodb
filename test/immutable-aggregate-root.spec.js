cf8c804becd1082ed29b925a78ecc68f95b4bb6a
Markus Tacker <Markus.Tacker@NordicSemi.no>
2017-08-10 16:08:34 +0200

    fix(dependencies): updated

--- test/immutable-aggregate-root.spec.js
+++ test/immutable-aggregate-root.spec.js
@@ -3,11 +3,12 @@
 import {ImmutableAggregateRoot, AggregateMeta} from '../src'
 import {ModelEvent} from '../src/model-event'
 import {expect} from 'chai'
+import {UnhandledDomainEventError} from '@rheactorjs/errors'
 
 describe('ImmutableAggregateRoot', () => {
   describe('.applyEvent()', () => {
     it('should throw an exception if not implemented', () => {
-      expect(() => ImmutableAggregateRoot.applyEvent(new ModelEvent('17', 'SomeEvent'))).to.throw(/UnhandledDomainEventError/)
+      expect(() => ImmutableAggregateRoot.applyEvent(new ModelEvent('17', 'SomeEvent'))).to.throw(UnhandledDomainEventError)
     })
   })
   describe('.meta', () => {
